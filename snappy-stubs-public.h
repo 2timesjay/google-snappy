10d05c1beacdef4f60506120a75657e62564c11e
Trond Norbye <trond.norbye@gmail.com>
2013-11-11 21:07:11 +0100

    fixup win32

--- snappy-stubs-public.h
+++ snappy-stubs-public.h
@@ -36,20 +36,13 @@
 #ifndef UTIL_SNAPPY_OPENSOURCE_SNAPPY_STUBS_PUBLIC_H_
 #define UTIL_SNAPPY_OPENSOURCE_SNAPPY_STUBS_PUBLIC_H_
 
-#ifdef WIN32
+#if defined(WIN32) && defined(_MSC_VER) && _MSC_VER < 1800
+#define NOMINMAX 1
 #include <windows.h>
 #endif
 
-#if 0
-#include <stdint.h>
-#endif
-
-#if 0
-#include <stddef.h>
-#endif
-
-#if 0
-#include <sys/uio.h>
+#ifndef WIN32
+#include <uio.h>
 #endif
 
 #define SNAPPY_MAJOR 1
@@ -62,7 +55,17 @@
 
 namespace snappy {
 
-#ifndef WIN32
+#if defined(WIN32) && defined(_MSC_VER) && _MSC_VER < 1800
+typedef __int64 ssize_t;
+typedef __int8 int8;
+typedef unsigned __int8 uint8;
+typedef __int16 int16;
+typedef unsigned __int16 uint16;
+typedef __int32 int32;
+typedef unsigned __int32 uint32;
+typedef __int64 int64;
+typedef unsigned __int64 uint64;
+#else
 typedef long ssize_t;
 typedef int8_t int8;
 typedef uint8_t uint8;
@@ -72,16 +75,6 @@ typedef int32_t int32;
 typedef uint32_t uint32;
 typedef int64_t int64;
 typedef uint64_t uint64;
-#else
-typedef long ssize_t;
-typedef signed char int8;
-typedef unsigned char uint8;
-typedef short int16;
-typedef unsigned short uint16;
-typedef int int32;
-typedef unsigned int uint32;
-typedef long long int64;
-typedef unsigned long long uint64;
 #endif
 
 typedef std::string string;
@@ -90,7 +83,7 @@ typedef std::string string;
   TypeName(const TypeName&);               \
   void operator=(const TypeName&)
 
-#if 1
+#ifdef WIN32
 // Windows does not have an iovec type, yet the concept is universally useful.
 // It is simple to define it ourselves, so we put it inside our own namespace.
 struct iovec {
